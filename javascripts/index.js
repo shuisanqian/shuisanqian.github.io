db3754143e329fded7362f1ebe1d66953051ca2e
水三千 <819332405@qq.com>
2016-06-28 11:16:25 +0800

    Commit

---
+++ javascripts/index.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	$('#dowebok').fullpage({
+		'verticalCentered': false,
+		'css3': true,
+		'slidesColor': ['#F0F2F4', '#fff', '#fff', '#fff'],
+		'navigation': true,
+		'navigationColor':'red',
+		'navigationPosition': 'right',
+		'navigationTooltips': ['home', 'project & works', 'skill', 'education'],
+		'afterRender':function(){
+			
+			
+		}
+	});
+
+})
